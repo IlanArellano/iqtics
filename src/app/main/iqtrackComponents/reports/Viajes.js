c19b722eb7eeecb907646c889f22d68f6db4ef36
IlanArellano <ilanarellano15@gmail.com>
2021-05-03 23:04:41 -0500

    Nuevas Vistas

--- src/app/main/iqtrackComponents/reports/Viajes.js
+++ src/app/main/iqtrackComponents/reports/Viajes.js
@@ -1,5 +1,181 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-export default function ReportRoute() {
-	return <div>Viajes</div>;
+const useStyles = makeStyles(theme => ({
+	container: {
+		display: 'flex'
+	},
+	formControl: {
+		margin: theme.spacing(1),
+		minWidth: 120
+	},
+	buttons: {
+		minWidth: 280,
+		margin: theme.spacing(1)
+	},
+	selectEmpty: {
+		marginTop: theme.spacing(2)
+	},
+	table: {
+		minWidth: '100%'
+	},
+	containTable: {
+		width: '100%',
+		marginLeft: '20px'
+	},
+	option: {
+		display: 'flex',
+		flexDirection: 'column'
+	}
+}));
+
+export default function ViajesRoute() {
+	const classes = useStyles();
+	const [route, setRoute] = useState('');
+	const [dispositivo, setDispositivo] = useState('');
+
+	const handleRoute = e => {
+		setRoute(e.target.value);
+	};
+
+	const handleDevice = e => {
+		setDispositivo(e.target.value);
+	};
+
+	function createData(name, calories, fat, carbs, protein) {
+		return { name, calories, fat, carbs, protein };
+	}
+
+	const rows = [
+		createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+		createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+		createData('Eclair', 262, 16.0, 24, 6.0),
+		createData('Cupcake', 305, 3.7, 67, 4.3),
+		createData('1', 356, 16.0, 49, 3.9),
+		createData('2', 356, 16.0, 49, 3.9),
+		createData('3', 356, 16.0, 49, 3.9),
+		createData('4', 356, 16.0, 49, 3.9),
+		createData('5', 356, 16.0, 49, 3.9),
+		createData('6', 356, 16.0, 49, 3.9),
+		createData('7', 356, 16.0, 49, 3.9),
+		createData('8', 356, 16.0, 49, 3.9),
+		createData('9', 356, 16.0, 49, 3.9),
+		createData('10', 356, 16.0, 49, 3.9),
+		createData('11', 356, 16.0, 49, 3.9),
+		createData('12', 356, 16.0, 49, 3.9),
+		createData('13', 356, 16.0, 49, 3.9),
+		createData('14', 356, 16.0, 49, 3.9),
+		createData('15', 356, 16.0, 49, 3.9),
+		createData('16', 356, 16.0, 49, 3.9),
+		createData('17', 356, 16.0, 49, 3.9),
+		createData('18', 356, 16.0, 49, 3.9),
+		createData('19', 356, 16.0, 49, 3.9),
+		createData('20', 356, 16.0, 49, 3.9)
+	];
+
+	return (
+		<div className={classes.container}>
+			<div className={classes.option}>
+				<div>
+					<FormControl variant="outlined" className={classes.formControl}>
+						<InputLabel id="Dispositivos" color="secondary">
+							Dispositivos
+						</InputLabel>
+						<Select
+							labelId="Dispositivos"
+							id="Dispositivos"
+							value={route}
+							onChange={handleRoute}
+							label="Dispositivos"
+						>
+							<MenuItem value={10}>Ten</MenuItem>
+							<MenuItem value={20}>Twenty</MenuItem>
+							<MenuItem value={30}>Thirty</MenuItem>
+						</Select>
+					</FormControl>
+					<FormControl variant="outlined" className={classes.formControl}>
+						<InputLabel id="Periodo" color="secondary">
+							Periodo
+						</InputLabel>
+						<Select
+							labelId="Periodo"
+							id="Periodo"
+							value={dispositivo || 'Hoy'}
+							onChange={handleDevice}
+							label="Periodo"
+						>
+							<MenuItem value="Hoy">Hoy</MenuItem>
+							<MenuItem value="Ayer">Ayer</MenuItem>
+							<MenuItem value="SemanaActual">Semana Actual</MenuItem>
+							<MenuItem value="SemanaAnterior">Semana Anterior</MenuItem>
+							<MenuItem value="MesActual">Mes Actual</MenuItem>
+							<MenuItem value="MesAnterior">Mes Anterior</MenuItem>
+							<MenuItem value="Personalizado">Personalizado</MenuItem>
+						</Select>
+					</FormControl>
+				</div>
+				<FormControl className={classes.buttons}>
+					<Button variant="outlined" color="primary">
+						Mostrar
+					</Button>
+					<Button variant="outlined" color="primary">
+						Exportar
+					</Button>
+					<Button variant="outlined" color="primary">
+						Reporte por correo
+					</Button>
+				</FormControl>
+			</div>
+			<div className={classes.containTable}>
+				<TableContainer component={Paper}>
+					<Table className={classes.table} aria-label="simple table">
+						<TableHead>
+							<TableRow>
+								<TableCell>Hora</TableCell>
+								<TableCell align="right">Hora de Inicio</TableCell>
+								<TableCell align="right">Odómetro Inicial</TableCell>
+								<TableCell align="right">Hora de Fin</TableCell>
+								<TableCell align="right">Odómetro Final</TableCell>
+								<TableCell align="right">Distancia</TableCell>
+								<TableCell align="right">Velocidad Promedio</TableCell>
+								<TableCell align="right">Velocidad Máxima</TableCell>
+								<TableCell align="right">Duración</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{rows.map(row => {
+								return (
+									<TableRow key={row.name}>
+										<TableCell component="th" scope="row">
+											{row.name}
+										</TableCell>
+										<TableCell align="right">{row.calories}</TableCell>
+										<TableCell align="right">{row.fat}</TableCell>
+										<TableCell align="right">{row.carbs}</TableCell>
+										<TableCell align="right">{row.protein}</TableCell>
+										<TableCell align="right">{row.protein}</TableCell>
+										<TableCell align="right">{row.protein}</TableCell>
+										<TableCell align="right">{row.protein}</TableCell>
+										<TableCell align="right">{row.protein}</TableCell>
+									</TableRow>
+								);
+							})}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			</div>
+		</div>
+	);
 }
--- src/app/main/iqtrackComponents/reports/index.js
+++ src/app/main/iqtrackComponents/reports/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
 import Ruta from './Route';
 import Eventos from './Events';
 import Viajes from './Viajes';
@@ -12,11 +14,19 @@ import Grafica from './Grafica';
 const useStyles = makeStyles(theme => ({
 	paper: {
 		position: 'absolute',
-		width: 400,
-		backgroundColor: '#ffffff',
+		minwidth: '70%',
+		maxHeight: '75vh',
+		backgroundColor: '#000000',
 		border: '2px solid #000',
-		boxShadow: theme.shadows[5],
+		boxShadow: theme.shadows[5]
+	},
+	content: {
 		padding: theme.spacing(2, 4, 3)
+	},
+	header: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center'
 	}
 }));
 
@@ -47,21 +57,25 @@ function ReportsModal({ display, open, setOpen }) {
 	};
 
 	return (
-		<Modal
-			open={open}
-			onClose={handleClose}
-			aria-labelledby="simple-modal-title"
-			aria-describedby="simple-modal-description"
-		>
+		<Modal open={open} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">
 			<div
 				style={{
 					top: `50%`,
 					left: `50%`,
-					transform: `translate(-50%, -50%)`
+					transform: `translate(-50%, -50%)`,
+					overflow: 'scroll'
 				}}
 				className={classes.paper}
 			>
-				<SetDisplay />
+				<div className={classes.header}>
+					<h3 style={{ color: '#ffffff', marginLeft: '15px' }}>{display}</h3>
+					<IconButton onClick={() => handleClose()}>
+						<Icon style={{ fontSize: 30, color: 'red' }}>cancel</Icon>
+					</IconButton>
+				</div>
+				<div className={classes.content}>
+					<SetDisplay />
+				</div>
 			</div>
 		</Modal>
 	);
